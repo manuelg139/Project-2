0e65414d512262f246a68fc8114581ca02d20f26
Dora O <dorozco2390@utexas.edu>
2021-04-10 11:29:58 -0500

    created seed data

---
+++ seeds/projectData.js
@@ -0,0 +1,18 @@
+const { Projects } = require('../models');
+
+const projectdata = [
+    {
+        title: 'ReadMe Generator',
+        media_link: 'https://youtu.be/LJDEDQDcHeM',
+        description: 'How to install and use readme generator'
+    },
+    {
+        title: 'Team Profile Generator',
+        media_link: 'https://youtu.be/GQsDzlJ7brE',
+        description: 'How to install and use profile generator'
+    },
+];
+
+const seedProject = () => Project.bulkCreate(projectdata);
+
+module.exports = seedProject;

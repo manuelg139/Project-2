340575bd3d2a605d7ac7d238985ca72f4d5fec01
Vanity <ktkyletran@gmail.com>
2021-04-12 19:53:56 -0500

    added code to handle email form

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+const emailFormHandler = async (event) => {
+  event.preventDefault();
+
+  const from = document.querySelector('#email-from').value.trim();
+  const to = document.querySelector('#email-to').value.trim();
+  const subject = document.querySelector('#email-subject').value.trim();
+  const content = document.querySelector('#email-content').value.trim();
+
+  if (from && to && subject && content) {
+    const response = await fetch('/api/emailers', {
+      method: 'POST',
+      body: JSON.stringify({ from, to, subject, content }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if(response.ok) {
+      document.location.replace('/')
+    }
+  }
+}

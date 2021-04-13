b0034bba6f367eb8817c4b50a2b8748a9ff2d0ca
Vanity <ktkyletran@gmail.com>
2021-04-12 19:23:59 -0500

    included emailerRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 const usersRoutes = require('./usersRoutes');
-/* const emailerRoutes = require('./emailerRoutes'); */
+const emailerRoutes = require('./emailerRoutes');
 const projectsRoutes = require('./projectsRoutes');
 const commentsRoutes = require('./commentsRoutes');
 
 router.use('/users', usersRoutes);
-/* router.use('/emailers', emailerRoutes); */
+router.use('/emailers', emailerRoutes);
 router.use('/projects', projectsRoutes);
 router.use('/comments', commentsRoutes);
 
